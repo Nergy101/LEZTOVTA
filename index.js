2c35244bf36067d16843b2b38e608322a48eeb34
Christian <chrisdvd@live.nl>
2021-10-20 22:19:55 +0200

    added emails 'storage'

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const port = 3000;
 const fileName = path.join(__dirname, 'data.json')
 
+app.use(express.json());
+
 app.get('/', function (_, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
@@ -18,24 +20,34 @@ app.get('/count', (_, res) => {
     }))
 })
 
-app.post('/increment', (_, res) => {
-    count = count + 1
-    writeFile(count)
+app.post('/increment', (req, res) => {
+    writeFile(req.body.email)
     res.sendStatus(200)
 })
 
-function writeFile(count) {
-    const data = JSON.stringify({
-        count
-    });
-    fs.writeFileSync(fileName, data);
+function writeFile(email) {
+    let data = getFile()
+
+    if (data.count) {
+        data.count += 1
+    } else {
+        data.count = 1;
+    }
+
+    if (!data.emails) {
+        data.emails = [];
+    }
+    data.emails.push(email)
+
+    data.emails = [...new Set(data.emails)]
+
+    fs.writeFileSync(fileName, JSON.stringify(data));
 }
 
 function getFile() {
     const rawdata = fs.readFileSync(fileName);
     return JSON.parse(rawdata);
 }
-
 
 console.log("Listening on ", port)
 app.listen(port)
